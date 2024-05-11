9d1c996a406ef1a38d80c2b43c91cfe7c7b212c4
Kevin <kevin.barr88@gmail.com>
2024-05-11 16:51:51 +0100

    dynamic routing

--- teched_wk8/src/app/posts/[id]/page.tsx
+++ teched_wk8/src/app/posts/[id]/page.tsx
@@ -1,8 +1,23 @@
-export default async function SinglePostPage ({ params }: { params: {id: number} }) {
-  console.log(params)
+async function generateMetadata({params}: {params: {id: number}})
+{
+    const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+
+    const post = await result.json();
+
+    return {
+        title: `Post: ${post.title}`,
+        description: `${post.description}`,
+    };
+}
+
+export default async function SinglePostPage ({ params }: { params: {id: number}}) {
+  const resp = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+  const post = await resp.json();
+
   return (
     <div>
       <h1>Single Post Page</h1>
+      <p>{post.title}</p>
     </div>
   )
 }
--- teched_wk8/src/app/posts/page.tsx
+++ teched_wk8/src/app/posts/page.tsx
@@ -1,15 +1,37 @@
-import Image from 'next/image'
 import Link from 'next/link'
 
-export default async function Posts () {
-    const resp = await fetch("https://jsonplaceholder.typicode.com/posts");
-    const posts = await resp.json();
+async function generateMetadata({params}: {params: {id: number}})
+{
+    const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
 
+    const post = await result.json();
+
+    return {
+        title: 'All of the posts. All. Of. Them.',
+        description: `Why can't I hold all of these posts!?`,
+    };
+}
+
+
+export default async function Posts ({
+  searchParams
+}: {
+  searchParams: { sort: string }
+}) {
+  const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
+  const posts = await resp.json()
+
+  if (searchParams.sort === 'desc') posts.reverse()
   return (
     <div>
       <h1>Posts</h1>
+
+      <Link href='/posts'>Sort Asc</Link>
+      <Link href='/posts?sort=desc'>Sort Desc</Link>
       <ul>
-        {posts.map((post: {id: number, title: string}) => (<li key={post.id}>{post.title}</li>))}
+        {posts.map((post: { id: number; title: string }) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
       </ul>
     </div>
   )
