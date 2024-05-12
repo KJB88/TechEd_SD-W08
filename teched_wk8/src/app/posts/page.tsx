654e85871f91766978f5f7a36ffe26c5500b1aff
Kevin <kevin.barr88@gmail.com>
2024-05-12 21:58:07 +0100

    insert posts

--- teched_wk8/src/app/posts/page.tsx
+++ teched_wk8/src/app/posts/page.tsx
@@ -1,9 +1,13 @@
 /* POSTS */
+
 import {
   getAllPostsWithUserJoin,
   addNewPost
 } from '@/app/ts/dbHandler'
+import { revalidatePath } from 'next/cache'
 import Link from 'next/link'
+import { useRef} from "react"
+
 export const metadata = {
   title: 'ZonePDA: All Posts',
   description: `View all Posts in ZonePDA.`
@@ -14,6 +18,7 @@ export default async function Posts ({
 }: {
   searchParams: { sort: string }
 }) {
+  const ref = useRef(null);
   const allPosts = await getAllPostsWithUserJoin()
 
   const postHTML = allPosts.map(post => (
@@ -34,7 +39,9 @@ export default async function Posts ({
     const header = formData.get("header") as string;
     const content = formData.get("content") as string;
      
-    const result = await addNewPost(header, content, 1, 1);
+    await addNewPost(header, content, 1, 1);
+
+    revalidatePath("/posts");
   }
 
   return (
@@ -42,9 +49,10 @@ export default async function Posts ({
       <h1 className='drop-shadow heading'>Posts</h1>
       <form className='flex flex-col border-b-white border-b-2 pb-2' action={submitPost}>
         <label htmlFor='title'>Title:</label>
-        <input type='text' name='header' />
+        <input className="text-black" type='text' name='header' />
         <label htmlFor='content'>Message:</label>
-        <input type='text' name='content' />
+        <input className="text-black" type='text' name='content' />
+        <button className="bg-gray-300 text-black mt-2">Submit Post</button>
       </form>
       <ul>{postHTML}</ul>
     </div>
